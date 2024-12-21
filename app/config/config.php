b3689fe19e845f93d70b2c3b9cc289e98d1a69dc
Albaxxe <128056031+Albaxxe@users.noreply.github.com>
2024-12-21 09:44:03 +0100

    Erreur lors de l'accès au site modification de plusieurs fichiers du back

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,7 @@
 <?php
 // File: app/config/config.php
 define('BASE_PATH', __DIR__ . '/../');
-define('BASE_URL', 'http://localhost/albaxe_blog/');
+define('BASE_URL', 'http://localhost/albaxe_blog/public');
 
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'albaxe_blog');
---
+++ app/views/auth/login.php
@@ -0,0 +1,11 @@
+<form method="post" action="/auth/login">
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" id="email" name="email" placeholder="Entrez votre email">
+    </div>
+    <div class="form-group">
+        <label for="password">Mot de passe</label>
+        <input type="password" class="form-control" id="password" name="password" placeholder="Entrez votre mot de passe">
+    </div>
+    <button type="submit" class="btn btn-primary">Se connecter</button>
+</form>
